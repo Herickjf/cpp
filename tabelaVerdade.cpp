d412064cd8e66e47062aea67fe0efdd9d872c23f
Herickjf <125290993+Herickjf@users.noreply.github.com>
2024-03-05 12:59:28 -0300

    Add files via upload

---
+++ tabelaVerdade.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** agrv){
+	
+	string fl;
+	
+	cout << "Type a logical equation: " << endl;
+	getline (cin, fl);
+	
+	cout << fl;
+	
+	return 0;
+}
+
