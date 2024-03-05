f806d011afb7b18137a5102dab3fb1a4e542ab67
Herickjf <125290993+Herickjf@users.noreply.github.com>
2024-03-05 13:28:33 -0300

    Update tabelaVerdade.cpp

--- tabelaVerdade.cpp
+++ tabelaVerdade.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
 
+
 using namespace std;
 
+int nDeLiterais(string fl);
+
+
 int main(int argc, char** agrv){
+	char simbolos[] = {'~', '^', 'v', '=', '-', '(', ')'};
 	
-	string fl;
+	string fl; //formula lógica
+	string ltr = "";
 	
-	cout << "Type a logical equation: " << endl;
+	cout << "Type a logical equation: ";
 	getline (cin, fl);
 	
-	cout << fl;
+	char literais[nDeLiterais(fl, simbolos, ltr)];
+	cout << ltr;
 	
 	return 0;
 }
 
+/*
+class Literal{
+	public:
+		char letra;
+		bool valor;
+};
+*/
+
+
+int nDeLiterais(string fl, char c[], string &ltr){
+	int n=0;
+	for(int i=0; i<fl.length(); i++){
+		if(fl[i] == ' ') continue;
+		else{
+			for(int j=0; j < sizeof(c); j++){
+				if(fl[i] != c[j]){
+					n++;	
+					ltr.append(fl[i]);
+				}
+			}
+		}
+	}
+	return n;
+}
